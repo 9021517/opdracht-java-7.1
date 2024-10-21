df27ed1159088ea79cbbdb3b0f18822f3f314ad8
9021517 <9021517@student.zadkine.nl>
2024-10-21 11:59:23 +0200

    Add files via upload

---
+++ opdracht java 7.1/app.js
@@ -0,0 +1,14 @@
+var Budget = 100;
+var product = 60;
+
+var ResultP = document.getElementById("saldo");
+
+if (Budget >= product) {
+    ResultP. innerHTML = "U heeft genoeg geld!";
+    ResultP. style.color = "green";
+}
+
+else {
+    ResultP. innerHTML = "Helaas, te weinig geldt";
+    ResultP. sttle.color= "red";
+}
